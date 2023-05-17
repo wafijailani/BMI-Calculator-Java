0283d0452c9564f024a96a5ea859240fd4958c0b
wafijailani <88270775+wafijailani@users.noreply.github.com>
2023-05-17 20:34:00 +0800

    Add files via upload

---
+++ src/bmi/calculator/BMICalculator.java
@@ -0,0 +1,11 @@
+package bmi.calculator;
+public class BMICalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
